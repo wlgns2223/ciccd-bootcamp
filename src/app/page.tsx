dde61bd8a0fc741e90a1972d91ed4a9f96d51b9d
ShimJiHoon <wlgns2223@gmail.com>
2025-07-04 13:25:40 +0900

    chore: deploy hook test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <div className="min-h-screen bg-gray-50 py-8">
       <main className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold text-center mb-8">CI 테스트</h1>
+        <h1 className="text-4xl font-bold text-center mb-8">CI/CD 테스트</h1>
         <div className="max-w-md mx-auto">
           <Counter />
         </div>
